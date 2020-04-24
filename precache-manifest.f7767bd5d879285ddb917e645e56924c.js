86cbbd71bc97377983c5d5aa94a19d3b62784da5
Terence Govender <terenceg@paycorp.co.za>
2020-04-24 08:14:54 +0200

    Updates

---
+++ precache-manifest.f7767bd5d879285ddb917e645e56924c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41e3b71131fb44456dfd217d8040c681",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "55821212987c15175500",
+    "url": "/monsters-rolodex/static/css/main.645096b1.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55821212987c15175500",
+    "url": "/monsters-rolodex/static/js/main.c8461c4b.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
